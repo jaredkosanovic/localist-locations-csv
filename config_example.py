b62cd39e7027bb579a84f76c88ea0b4230b82a7e
Jared Kosanovic <jared@Jareds-MacBook-Pro.local>
2016-12-14 12:19:50 -0800

    updated to use locations frontend and api gateway

--- config_example.py
+++ config_example.py
@@ -1,3 +1,3 @@
-locations_url = "https://host:port/api/v1/locations/arcgis"
-user = "super"
-password = "secret"
+locations_url = "https://host/v1/locations"
+client_id = ""
+client_secret = ""
--- get_locations_csv.py
+++ get_locations_csv.py
@@ -1,23 +1,36 @@
 import json, requests, ssl, csv, re, time
 from config import *
 
-locations_request = requests.get(locations_url, verify=False, auth=(user, password))
-response = locations_request.json()
+def get_locations():
+    access_token_url = locations_url + "/token"
+    post_data = {'client_id': client_id, 
+        'client_secret': client_secret, 
+        'grant_type': 'client_credentials'}
+    token_request = requests.post(access_token_url, data=post_data)
+    token_response = token_request.json()
+    access_token = 'Bearer ' + token_response["access_token"]
+    
+    headers = {'Authorization': access_token}
+    query_params = {'campus': 'corvallis', 'type':'building', 'page[size]': 9999}
+    locations_request = requests.get(locations_url, headers=headers, params=query_params)
+    return locations_request.json()
 
-locations_csv = csv.writer(open("osu-corvallis-locations-" + time.strftime("%m-%d-%Y") + ".csv", "wb+"))
+locations_response = get_locations()
+
+locations_csv = csv.writer(open("osu-corvallis-locations-" + time.strftime("%m-%d-%Y") + ".csv", "w"))
 locations_csv.writerow(['Name', 'Description', 
     'Type', 'URL', 'Address', 'City', 'State', 
     'Photo URL', 'Longitude', 'Latitude'])
 
-for location in response['data']:
+for location in locations_response['data']:
     attributes = location['attributes']
-
+    print attributes
     # Strip summary text of HTML tags
     if attributes['summary'] is not None:
         summary = re.sub("<.*?>", "", attributes['summary'].encode('utf-8').strip())
     else:
         summary = None
-
+    print summary
     # Images are nicer than thumbnails.  
     if attributes['images']:
         image = attributes['images'][0] 
