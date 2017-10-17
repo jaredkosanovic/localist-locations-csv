f64b081e4dd8087d14c3ff9b8e373891295fc92f
Jared Kosanovic <kosanovj@oregonstate.edu>
2017-10-17 15:43:23 -0700

    add campuses

--- config_example.py
+++ config_example.py
@@ -2,3 +2,4 @@ locations_url = "https://host/v1/locations"
 token_api = "https://api.oregonstate.edu/oauth2/token"
 client_id = ""
 client_secret = ""
+campuses = ['corvallis', 'cascades', 'hmsc', 'other']
